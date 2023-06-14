1b3694de1200f8a514d7f27bbaecd9dc9cfb1e16
kishor-7008 <kishor@hminnovance.com>
2023-06-15 00:59:09 +0530

    ews

--- src/component/employee/AddBook.js
+++ src/component/employee/AddBook.js
@@ -13,10 +13,8 @@ export default function AddBook() {
                             <h5 class="text-center mb-4">Powering world-class companies</h5>
                             <form class="form-card" onsubmit="event.preventDefault()">
                                 <div class="row justify-content-between text-left">
-                                    <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Category<span class="text-danger"> *</span></label>  <select class="form-select" aria-label="Disabled select example"
-                                        id="fname" name="fname">
-
-
+                                    {/* <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Category<span class="text-danger"> *</span></label>  <select class="form-select" aria-label="Disabled select example"
+                                        id="fname" name="fname ">
                                         <option value="Literary Fiction">Literary Fiction</option>
                                         <option value="History">History</option>
                                         <option value="Religion and Mythology">Religion and Mythology</option>
@@ -31,7 +29,23 @@ export default function AddBook() {
                                         <option value="Travel and Places">Travel and Places</option>
 
 
-                                    </select> </div>
+                                    </select> </div> */}
+                                    <div class="form-group col-sm-6 flex-column d-flex"><label class="form-control-label px-3">Book name<span class="text-danger"> *</span></label> <select style={{height:"6vh",marginTop:"6px"}}>
+                                        
+                                    <option value="Literary Fiction">Literary Fiction</option>
+                                        <option value="History">History</option>
+                                        <option value="Religion and Mythology">Religion and Mythology</option>
+                                        <option value="Historical Fiction">Historical Fiction</option>
+                                        <option value="Mythological Fiction">Mythological Fiction</option>
+                                        <option value="Thriller and Mystery">Thriller and Mystery</option>
+                                        <option value="Sci-Fi and Fantasy">Sci-Fi and Fantasy</option>
+                                        <option value="Humour">Humour</option>
+                                        <option value="Chick-lit">Chick-lit</option>
+                                        <option value="Biography and Memoir">Biography and Memoir</option>
+                                        <option value="Self-Help">Self-Help</option>
+                                        <option value="Travel and Places">Travel and Places</option>
+                                        </select></div>
+                                    
                                     <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Book name<span class="text-danger"> *</span></label> <input type="text" id="lname" name="lname" placeholder="Enter your last name" onblur="validate(2)" /> </div>
                                 </div>
                                 <div class="row justify-content-between text-left">
--- src/component/employee/EmployeeHomeCart.js
+++ src/component/employee/EmployeeHomeCart.js
@@ -3,6 +3,8 @@ import axios from "axios";
 import '../students/home.css'
 import Slider from "../students/Slider";
 import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import './employeeModal.css'
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
@@ -10,17 +12,15 @@ const BASE_URL = "https://movie-list.alphacamp.io";
 const INDEX_URL = BASE_URL + "/api/v1/movies/";
 const POSTER_URL = BASE_URL + "/posters/";
 const MOVIES_PER_PAGE = 12;
-
 const style = {
     position: 'absolute',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
+    // width: 400,
+
+    // boxShadow: 24,
+
 };
 const EmployeeHomeCart = () => {
     const [movies, setMovies] = useState([]);
@@ -30,8 +30,8 @@ const EmployeeHomeCart = () => {
     const [currentSearch, setCurrentSearch] = useState("off");
     const [errorMode, setErrorMode] = useState(false);
     const [keyword, setKeyword] = useState("");
-    const[open, setOpen] = React.useState(false);
-    const handleOpen = () => {setOpen(true);console.log("jgvg ghvhnghnvh")};
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => { setOpen(true); console.log("jgvg ghvhnghnvh") };
     const handleClose = () => setOpen(false);
     useEffect(() => {
         axios.get("https://book-admin.onrender.com/getBook")
@@ -65,7 +65,7 @@ const EmployeeHomeCart = () => {
                                     className="btn bg-warning " data-id={item.id}
                                     onClick={() => console.log("hk")}
                                 >
-                                    Datails 
+                                    Datails
 
 
 
@@ -75,7 +75,7 @@ const EmployeeHomeCart = () => {
 
 
 
-                         {/* on click to show modal */}
+                                    {/* on click to show modal */}
 
 
 
@@ -83,7 +83,7 @@ const EmployeeHomeCart = () => {
                                 <button
                                     className="btn bg-dark btn-add-favorite"
                                     data-id={item.id}
-                                    onClick={ handleOpen }
+                                    onClick={handleOpen}
                                 >
                                     <i class="fa-solid fa-trash" style={{ color: "#ea2610" }}></i>
 
@@ -96,20 +96,111 @@ const EmployeeHomeCart = () => {
                                     onClose={handleClose}
                                     aria-labelledby="modal-modal-title"
                                     aria-describedby="modal-modal-description"
+                                    hideBackdrop="true"
+                                    disableBackdropClick
                                 >
-                                    <Box sx={style}>
-                                        <Typography id="modal-modal-title" variant="h6" component="h2">
-                                            Text in a modal
-                                        </Typography>
-                                        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                                            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                                        </Typography>
+                                    <Box sx={style} >
+
+
+
+
+
+                                        <div class=" mx-auto">
+                                            <div class="card mt-2 mx-auto p-4 bg-light">
+                                                <div class="card-body bg-light">
+
+                                                    <div class="container">
+                                                        <form id="contact-form" role="form">
+
+
+
+                                                            <div class="controls">
+
+                                                                <div class="row">
+                                                                    <div class="col-md-6">
+                                                                        <div class="form-group">
+                                                                            <label for="form_name">Book Category *</label>
+                                                                            <input id="form_name" type="text" name="name" class="form-control" placeholder="Please enter your firstname *" required="required" data-error="Firstname is required." />
+
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="col-md-6">
+                                                                        <div class="form-group">
+                                                                            <label for="form_lastname">Book Name *</label>
+                                                                            <input id="form_lastname" type="text" name="surname" class="form-control" placeholder="Please enter your lastname *" required="required" data-error="Lastname is required." />
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="row">
+                                                                    <div class="col-md-6">
+                                                                        <div class="form-group">
+                                                                            <label for="form_email">Image URL *</label>
+                                                                            <input id="form_email" type="text" name="email" class="form-control" placeholder="Please enter your email *" required="required" data-error="Valid email is required." />
+
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="col-md-6">
+                                                                        <div class="form-group">
+                                                                            <label for="form_need">Please specify your need *</label>
+                                                                            <select id="form_need" name="need" class="form-control selectText" required="required" data-error="Please specify your need." style={{ width: "250px", height: "5.5vh" }}>
+                                                                                <option value="" selected disabled style={{ fontSize: "14px", fontWeight: "bold" }}>--Select Your Issue--</option>
+                                                                                <option style={{ fontSize: "14px", fontWeight: "bold" }}>Request Invoice for order</option>
+                                                                                <option style={{ fontSize: "14px", fontWeight: "bold" }}>Request order status</option>
+                                                                                <option style={{ fontSize: "14px", fontWeight: "bold" }}>Haven't received cashback yet</option>
+                                                                                <option style={{ fontSize: "14px", fontWeight: "bold" }}>Other</option>
+                                                                            </select>
+
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="row">
+                                                                    <div class="col-md-6">
+                                                                        <div class="form-group">
+                                                                            <label for="form_email">Author *</label><br />
+                                                                            <input id="form_email" type="text" name="email" class="form-control" placeholder="Please enter your email *" required="required" data-error="Valid email is required." />
+
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="col-md-6">
+
+                                                                        <div class="form-group">
+                                                                            <label for="form_message">Description *</label>
+                                                                            <textarea id="form_message" name="message" class="form-control" placeholder="Write your message here." required="required" data-error="Please, leave us a message."></textarea
+                                                                            >
+                                                                        </div>
+
+                                                                    </div>
+
+
+                                                                    <div class="col-md-12">
+
+                                                                        <input type="submit" class="btn btn-success btn-send  pt-2 btn-block
+                                                                       " value="Send Message" />
+
+                                                                    </div>
+
+                                                                </div>
+
+
+                                                            </div>
+                                                        </form>
+                                                    </div>
+                                                </div>
+
+
+                                            </div>
+
+                                        </div>
+
+
+
+
                                     </Box>
                                 </Modal>
 
 
 
-                                
+
 
                             </div>
                         </div>
@@ -126,7 +217,7 @@ const EmployeeHomeCart = () => {
                     <li className="list-group-item" key={item.id}>
                         <h5 className="list-title">{item.name}</h5>
                         <div className="">
-                            <button 
+                            <button
                                 type="button"
                                 className="btn btn-primary btn-show-movie"
                                 // data-bs-toggle="modal"
